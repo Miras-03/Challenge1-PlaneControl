199229ad5fdedb64b3dadd15113db5868297d089
Miras-03 <bolatbek.miras@bk.ru>
2023-11-15 20:56:36 +0300

    First commit

---
+++ Challenge1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform plane;
+    private Vector3 offset;
+
+    private void Start() => offset = Camera.main.transform.position;
+
+    private void FixedUpdate() => transform.position = plane.transform.position + offset;
+}
---
+++ Challenge1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(5, 20)]
+    [SerializeField] private float speed = 15;
+
+    private const float rotationSpeed = 2;
+    private float verticalInput = 0;
+
+    private void Update() => verticalInput = Input.GetAxis("Vertical");
+
+    private void FixedUpdate()
+    {
+        transform.Translate(CalculateTranslate());
+        transform.Rotate(CalculateRotate());
+    }
+
+    private Vector3 CalculateTranslate() => Vector3.forward *speed * Time.fixedDeltaTime;
+
+    private Vector3 CalculateRotate() => Vector3.left * verticalInput * -rotationSpeed;
+}
